56143c81680686d98d20ad83f62bc3d05c4c1470
VinayKanithi <vinaykanithi9989@gmail.com>
2025-01-05 19:30:56 +0530

    Update Reverse an array.py

--- Reverse an array.py
+++ Reverse an array.py
@@ -1,13 +1,4 @@
-# n=int(input())
-# a=list(map(int,input().split()))[:n]
-# b=a[::-1]
-# print(b)
-import string
 n=int(input())
 a=list(map(int,input().split()))[:n]
-b=a
-c=a.sort()
-if a==c:
-    print("True")
-else:
-    print("False")
+b=a[::-1]
+print(b)
